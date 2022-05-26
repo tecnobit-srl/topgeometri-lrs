3a3ee05f87b1fc27a48c9d8eaa083c791d6a5657
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2022-05-26 17:47:59 +0200

    Statement observer

---
+++ app/Observers/StatementObserver.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Observers;
+
+use Illuminate\Support\Facades\Log;
+use Trax\XapiStore\Stores\Statements\Statement;
+
+
+class StatementObserver
+{
+    /**
+     * Handle the Statement "created" event.
+     *
+     * @param  \App\Models\Statement  $statement
+     * @return void
+     */
+    public function created(Statement $statement)
+    {
+        Log::info($statement);
+    }
+
+    /**
+     * Handle the Statement "updated" event.
+     *
+     * @param  \App\Models\Statement  $statement
+     * @return void
+     */
+    public function updated(Statement $statement)
+    {
+        //
+    }
+
+    /**
+     * Handle the Statement "deleted" event.
+     *
+     * @param  \App\Models\Statement  $statement
+     * @return void
+     */
+    public function deleted(Statement $statement)
+    {
+        //
+    }
+
+    /**
+     * Handle the Statement "restored" event.
+     *
+     * @param  \App\Models\Statement  $statement
+     * @return void
+     */
+    public function restored(Statement $statement)
+    {
+        //
+    }
+
+    /**
+     * Handle the Statement "force deleted" event.
+     *
+     * @param  \App\Models\Statement  $statement
+     * @return void
+     */
+    public function forceDeleted(Statement $statement)
+    {
+        //
+    }
+}
