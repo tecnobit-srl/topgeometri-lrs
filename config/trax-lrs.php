79ce4a35151e118f5aef80ce00736943ab307a1c
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2022-06-10 16:07:14 +0200

    fix endpoint

--- config/trax-lrs.php
+++ config/trax-lrs.php
@@ -7,6 +7,6 @@ return [
         'prod' => env('LRS_SYNC_PROD', false),
         'dev_url' => env('LRS_SYNC_DEV_URL', 'https://devadmin.topgeometri.it'),
         'prod_url' => env('LRS_SYNC_PROD_URL', 'https://topgeometri.it'),
-        'endpoint' => env('LRS_SYNC_ENDPOINT', '/api/lrs-statements'),
+        'endpoint' => env('LRS_SYNC_ENDPOINT', '/api/courses/lrs-statements'),
     ]
 ];
