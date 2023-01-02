af34e568db4ed6588c513d2125cb0b75818f48fd
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2023-01-02 17:24:58 +0100

    config to prod

--- config/trax-lrs.php
+++ config/trax-lrs.php
@@ -3,10 +3,10 @@
 return [
     'version' => '2.0.1',
     'lrs_sync' => [
-        'dev' => env('LRS_SYNC_DEV', true),
-        'prod' => env('LRS_SYNC_PROD', false),
+        'dev' => env('LRS_SYNC_DEV', false),
+        'prod' => env('LRS_SYNC_PROD', true),
         'dev_url' => env('LRS_SYNC_DEV_URL', 'https://devadmin.topgeometri.it'),
-        'prod_url' => env('LRS_SYNC_PROD_URL', 'https://topgeometri.it'),
+        'prod_url' => env('LRS_SYNC_PROD_URL', 'https://admin.topgeometri.it'),
         'endpoint' => env('LRS_SYNC_ENDPOINT', '/api/courses/lrs-statements'),
     ]
 ];
